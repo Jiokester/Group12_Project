f4bfbf1bc644a6c34888d64f45d5fb086b519e80
RileyConlon <rpconlon@mail.csuchico.edu>
2023-11-28 15:04:22 -0800

    Merge Conflicts fix

--- Group_12 Game Project/Assets/Enemy.cs
+++ Group_12 Game Project/Assets/Enemy.cs
@@ -2,12 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletBill : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
-    public int speed;
-
-    public GameObject spawnPoint;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +13,6 @@ public class BulletBill : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        bulletBill();
+        
     }
-    private void bulletBill()
-    {
-        transform.position += Vector3.left * speed * Time.deltaTime;
-    }
-
 }
