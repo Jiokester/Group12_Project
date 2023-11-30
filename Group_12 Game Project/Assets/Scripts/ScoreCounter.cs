7245a5fd4651fc06063b7d4a0c7473209b9c7f10
Jiokester <148890522+Jiokester@users.noreply.github.com>
2023-11-30 08:58:41 -0800

    more stuff

--- Group_12 Game Project/Assets/Scripts/ScoreCounter.cs
+++ Group_12 Game Project/Assets/Scripts/ScoreCounter.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+
+/*
+ * Author: [Ihejirika, Chijioke]
+ * Last Updated: [11/29/2023]
+ * [Keeps track of players score]
+ */
 public class ScoreCounter : MonoBehaviour
 {
     public TMP_Text scoreText;
@@ -13,4 +19,5 @@ public class ScoreCounter : MonoBehaviour
     {
         scoreText.text = "Score:" + playerController.totalScore.ToString();
     }
+        
 }
